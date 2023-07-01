335e1052db971d9af67f909a0ad660acbd0bc9e5
yauhenmakarevich <yauhenmakarevich@gmail.com>
2023-07-01 16:17:52 +0300

    feat: add solution

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -522,8 +522,17 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const scope = new Map();
+  array.map((value) => {
+    if (scope.has(keySelector(value))) {
+      scope.get(keySelector(value)).push(valueSelector(value));
+    } else {
+      scope.set(keySelector(value), [valueSelector(value)]);
+    }
+    return scope;
+  });
+  return scope;
 }
 
 
@@ -540,8 +549,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.reduce((acc, value) => [...acc, ...childrenSelector(value)], []);
 }
 
 
@@ -557,8 +566,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, index) => acc[index], arr);
 }
 
 
@@ -580,8 +589,13 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length % 2 === 0) {
+    const len = arr.length / 2;
+    return [...arr.slice(len, arr.length), ...arr.slice(0, len)];
+  }
+  const len = (arr.length - 1) / 2;
+  return [...arr.slice(len + 1, arr.length), arr[len], ...arr.slice(0, len)];
 }
 
 
